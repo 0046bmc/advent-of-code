51869ee477a80c3d19bc65ebad7b4789f14fe5a5
Magnus Ahlstrom <magnus@atuin.se>
2023-12-09 12:02:24 +0100

    Reset for phaoc

--- src/Event/Year2015/Helpers/TSP.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Event\Year2015\Helpers;
-
-class TSP
-{
-
-}
--- src/MapBaseInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App;
-
-interface MapBaseInterface
-{
-    public function checkMinMax(string $plane, int $min, int $max): void;
-}
